023cc712baf7e5ca5a696eb0c17b708a8c6f0913
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-08-13 09:56:58 +0200

    source code manual reformat

--- main.php
+++ main.php
@@ -1,28 +1,26 @@
 <?php
+
 $words = explode(" ", "print 1 do print add 1 2 print 4 end");
-$defs = [
-    "print" => 1,
-    "add"   => 2,
-];
-function phrase_length($word_index,$words,$defs){
-    $word = $words[$word_index];
+$defs = [ "print" => 1, "add"   => 2 ];
+
+$word_index = 2;
+echo phrase_length($word_index,$words,$defs);
+
+function phrase_length($word_index, $words, $defs) {
+    $word = $words[ $word_index ];
     $length = 1;
-    if(is_numeric($word)){
-    }elseif( $word=="do"){
-        while(true){
-            if($words[$word_index+$length] == "end" ){
-                $length += 1;
-                break;
-            }
-            $length += phrase_length($word_index+$length,$words,$defs);
-        }
-    }else{
+
+    if( is_numeric($word) ) {
+        
+    } elseif( $word=="do") { while(true) {
+        if($words[$word_index+$length] == "end" ) {
+            $length += 1; break;
+        } else $length += phrase_length($word_index+$length,$words,$defs);
+    } } else {
         $argument_length = $defs[$word];
-        for($arg_index=0;$arg_index<$argument_length;$arg_index++){
-            $length += phrase_length($word_index+$length,$words,$defs);
+        for($arg_index=0; $arg_index<$argument_length; $arg_index++) {
+            $length += phrase_length($word_index+$length, $words, $defs);
         }
     }
     return $length;
 }
-$word_index = 2;
-echo phrase_length($word_index,$words,$defs);

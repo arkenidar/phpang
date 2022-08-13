cf51e2aee2f4dedd58cc04ba8ae99e7a09969a53
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-08-13 08:47:47 +0200

    using closures, with reference, also one recursive

--- main.php
+++ main.php
@@ -1,26 +1,25 @@
 <?php
-
 $words = explode(" ", "print 1 do print add 1 2 print 4 end");
 $defs = [ "print" => 1, "add"   => 2 ];
-
-$word_index = 2;
-echo phrase_length($word_index,$words,$defs);
-
-function phrase_length($word_index, $words, $defs) {
+$phrase_length = function($word_index) use (&$words, &$defs, &$phrase_length) {
     $word = $words[ $word_index ];
     $length = 1;
+    $next_index = function() use (&$word_index, &$length) { return $word_index+$length; } ;
 
     if( is_numeric($word) ) {
         
     } elseif( $word=="do") { while(true) {
-        if($words[$word_index+$length] == "end" ) {
+        if($words[$next_index()] == "end" ) {
             $length += 1; break;
-        } else $length += phrase_length($word_index+$length,$words,$defs);
+        } else $length += $phrase_length($next_index());
     } } else {
         $argument_length = $defs[$word];
         for($arg_index=0; $arg_index<$argument_length; $arg_index++) {
-            $length += phrase_length($word_index+$length, $words, $defs);
+            $length += $phrase_length($next_index());
         }
     }
     return $length;
-}
+};
+
+$word_index = 2;
+echo $phrase_length($word_index);
